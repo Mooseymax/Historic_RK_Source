dbe5a3380d8433ef3f0141097dd3b52829ed9032
Wolf <mooseandham@gmail.com>
2015-10-28 22:54:55 +0000

    Uploading compiled files

---
+++ 1. Compiler.bat
@@ -0,0 +1,2 @@
+"C:/Program Files/Java/jdk1.8.0_60/bin/javac.exe" -d bin -cp lib/*;src src\com\runekeep\GameServer.java
+pause
---
+++ 1. Run.bat
@@ -0,0 +1,4 @@
+@echo off
+title Runekeep
+"C:/Program Files/Java/jdk1.8.0_60/bin/java.exe" -server -Xmx3024m -cp bin;lib/* com.runekeep.GameServer
+pause
